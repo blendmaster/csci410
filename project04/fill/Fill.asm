0f79fed767037256660955d6ec3a0988904ed5d1
Steven Ruppert <blendmaster@gmail.com>
2012-02-13 23:39:25 -0700

    finished fill/Fill.asm

--- project04/fill/Fill.asm
+++ project04/fill/Fill.asm
@@ -9,3 +9,53 @@
 // the screen should be cleared.
 
 // Put your code here.
+
+//store position in ram0
+@SCREEN
+D = A
+@0
+M = D
+(loop)
+	//fill or clear based on keyboard
+	@KBD
+	D = M
+	@clear
+	D;JEQ
+(fill)
+	// read ram0 to D
+	@0
+	D = M
+	//blacken screen
+	A = D 
+	M = 0
+	M = !M
+(inc)
+	//increment counter and store
+	D = D + 1
+	@0
+	M = D
+	//reset to @screen if at end
+	@8192 //number of blocks in screen 
+	D = D - A
+	@SCREEN
+	D = D - A
+	@skipreset
+	D;JLE
+	//ram0 to screen
+	@screen
+	D = A
+	@0
+	M = D
+(skipreset)
+	@loop
+	A;JMP
+(clear)
+	// read ram0 to D
+	@0
+	D = M
+	//clear screen
+	A = D 
+	M = 0
+	//back to increment
+	@inc
+	A; JMP
